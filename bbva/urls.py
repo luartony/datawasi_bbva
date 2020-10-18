3568e8dbfef8ac530217f40f392d7a2e9a7cd5ba
Luigi Arroyo <luartony@gmail.com>
2020-10-18 09:09:10 -0500

    form

--- bbva/urls.py
+++ bbva/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('credito', views.CreditoView.as_view(), name='credito'),
+    path('consulta', views.ConsultaView.as_view(), name='consulta'),
+    path('resultado', views.ResultadoView.as_view(), name='resultado'),
 ]
--- bbva/views.py
+++ bbva/views.py
@@ -4,10 +4,29 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 class IndexView(generic.ListView):
-    template_name = 'bbva/index.html'
-    context_object_name = 'latest_question_list'
-    
+	template_name = 'bbva/index.html'
+	context_object_name = 'index'
 
-    def get_queryset(self):
-    	"""Return the last five published questions."""
-    	return User.objects.all()
+	def get_queryset(self):
+		return User.objects.all()
+
+class CreditoView(generic.ListView):
+	template_name = 'bbva/credito.html'
+	context_object_name = 'solicita credito'
+
+	def get_queryset(self):
+		return User.objects.all()
+
+class ConsultaView(generic.ListView):
+	template_name = 'bbva/consuta.html'
+	context_object_name = 'consulta'
+
+	def get_queryset(self):
+		return User.objects.all()
+
+class ResultadoView(generic.ListView):
+	template_name = 'bbva/resultado.html'
+	context_object_name = 'resultado'
+
+	def get_queryset(self):
+		return User.objects.all()
