c09fcd7064c914a1c5e0c9dd749cca63f0343c63
Luigi Arroyo <luartony@gmail.com>
2020-10-17 21:12:44 -0500

    all remote

--- datawasi_bbva/settings.py
+++ datawasi_bbva/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8%x)_^8ad)wl4(=pbnyl=mfl27gf5i#4-1mjlpm+c@c907_dyc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '*']
 
 
 # Application definition
